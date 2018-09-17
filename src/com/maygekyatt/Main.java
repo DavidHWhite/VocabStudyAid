d2f395b4113ac6364117a05eb531db84aac03e48
DAVIDDESKTOP\David White <davidwhite@triad.rr.com>
2018-09-17 18:34:19 -0400

    Completed
    Signed-off-by: DAVIDDESKTOP\David White <davidwhite@triad.rr.com>

--- src/com/maygekyatt/Main.java
+++ src/com/maygekyatt/Main.java
@@ -1,20 +1,128 @@
 package com.maygekyatt;
 
-import org.ini4j.Ini;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-	    try {
+	public static void main(String[] args) {
+		//Create scanner looking for text input from user
+		Scanner scan = new Scanner(System.in);
 
-		    Ini vocabList = new Ini(new File("list1.ini"));
-		    Morph m1 = new Morph("Anthropo-", "human being", "anthropology", "the study of human culture", "philanthropic", "seeking to provide for the welfare of others");
+		System.out.println("What list do you want to use?");
+		String s = scan.nextLine();
+
+		VocabList vocabList = new VocabList(s + ".ini");
+
+		while (vocabList.getMorphList().size() != 0) {
+			System.out.println();
+			quiz(vocabList, scan, vocabList.getMaxTries());
+		}
+	}
 
 
-	    } catch (IOException e) {
-		    e.printStackTrace();
-	    }
-    }
+	private static void quiz(@NotNull VocabList vocabList, @NotNull Scanner scan, int maxTries) {
+		//region Choose a random morph
+		Random rand = new Random();
+		VocabList.Morph quizSubject;
+		ArrayList<VocabList.Morph> morphs = vocabList.getMorphList();
+		quizSubject = morphs.get(rand.nextInt(morphs.size()));
+		boolean isEverythingPerfect = true;
+		boolean isThisCorrect;
+		//endregion
+
+		//region Ask for and check definition of the morph
+		do {
+			System.out.println("What is the definition of \"" + quizSubject.morph + "\"?");
+			String in = scan.nextLine().toLowerCase();
+			if (in.equals(quizSubject.morphDef.toLowerCase())) {
+				System.out.print("Correct! ");
+				isThisCorrect = true;
+			} else {
+				System.out.println("Unfortunately, the answer we were looking for was \"" + quizSubject.morphDef + "\". Please try again.");
+				changeScore(isEverythingPerfect, quizSubject, vocabList, maxTries);
+				isEverythingPerfect = false;
+				isThisCorrect = false;
+			}
+		} while (!isThisCorrect);
+		//endregion
+
+		//region Ask for and check each of the 2 words and their definitions
+		int prevWord = 0;
+		for (int goTwice = -2; goTwice < 0; goTwice++) {
+			//region Ask for and check one word
+			do {
+				if (goTwice == -2) System.out.println("What is one of the words that uses this morph?");
+				else System.out.println("What is the other word that uses this morph?");
+
+				String in = scan.nextLine().toLowerCase();
+				if (in.equals(quizSubject.word1.toLowerCase()) && prevWord != 1) {
+					isThisCorrect = true;
+					prevWord = 1;
+				} else if (in.equals(quizSubject.word2.toLowerCase()) && prevWord != 2) {
+					isThisCorrect = true;
+					prevWord = 2;
+				} else {
+					System.out.println("Unfortunately, that answer was incorrect. Make sure that you remember both \"" + quizSubject.word1 + "\" or \"" + quizSubject.word2 + "\". Please try again.");
+					changeScore(isEverythingPerfect, quizSubject, vocabList, maxTries);
+					isEverythingPerfect = false;
+					isThisCorrect = false;
+				}
+			} while (!isThisCorrect);
+			//endregion
+
+			//region Ask for and check the word's definition
+			System.out.print("Correct!");
+			System.out.print(" Do you know this word's definition? Press enter once you've thought of it.");
+			//region Wait for enter keypress
+			try {//noinspection ResultOfMethodCallIgnored
+				System.in.read();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			//endregion
+			System.out.println("The actual definition is \"" + quizSubject.getWordDef(prevWord) + "\". Did you get it right? (y/n)");
+			if (scan.nextLine().toLowerCase().equals("y")) {
+				System.out.println("Great job!");
+			} else {
+				System.out.println("Make sure you remember next time!");
+				changeScore(isEverythingPerfect, quizSubject, vocabList, maxTries);
+				isEverythingPerfect = false;
+			}
+			//endregion
+		}
+		//endregion
+
+		//region If no mistakes were made, increase score by 1
+		if (isEverythingPerfect) {
+			quizSubject.score++;
+			vocabList.getListIni().put(quizSubject.morph, "score", quizSubject.score);
+			try {
+				vocabList.getListIni().store(vocabList.getFile());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//endregion
+
+		//region If score has reached 0, remove from morphList
+		if (quizSubject.score >= 0)
+			morphs.remove(quizSubject);
+		//endregion
+	}
+
+	private static void changeScore(boolean isCorrect, VocabList.Morph quizSubject, VocabList vocabList, int maxTries) {
+		try {
+			if (isCorrect) {
+				quizSubject.score = Math.max(maxTries, --quizSubject.score);
+				vocabList.getListIni().put(quizSubject.morph, "score", quizSubject.score);
+				vocabList.getListIni().store(vocabList.getFile());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/com/maygekyatt/VocabList.java
+++ src/com/maygekyatt/VocabList.java
@@ -4,41 +4,86 @@ import org.ini4j.Ini;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
 
-public class VocabList {
+class VocabList {
 	/**
 	 * Nested inner class Morph
 	 * Used to store all data associated with an entry in the vocab list
 	 */
 	class Morph {
-		private String morph, morphDef, word1, word1Def, word2, word2Def;
+		String morph, morphDef, word1, word1Def, word2, word2Def;
+		int score;
+
+		String getWordDef(int word) {
+			if (word == 1) {
+				return word1Def;
+			} else {
+				return word2Def;
+			}
+		}
 
 		/**
 		 * Constructor
-		 * @param morph Morph to be defined
+		 *
+		 * @param morph    Morph to be defined
 		 * @param morphDef Definition of morph
-		 * @param word1 First word that uses the morph
+		 * @param word1    First word that uses the morph
 		 * @param word1Def Definition of word1
-		 * @param word2 Second word that uses the morph
+		 * @param word2    Second word that uses the morph
 		 * @param word2Def Definition of word2
 		 */
-		public Morph(String morph, String morphDef, String word1, String word1Def, String word2, String word2Def) {
+		Morph(String morph, String morphDef, String word1, String word1Def, String word2, String word2Def, int score) {
 			this.morph = morph;
 			this.morphDef = morphDef;
 			this.word1 = word1;
 			this.word1Def = word1Def;
 			this.word2 = word2;
 			this.word2Def = word2Def;
+			this.score = score;
 		}
 	}
 
+	ArrayList<Morph> getMorphList() { return morphList; }
 
+	private ArrayList<Morph> morphList = new ArrayList<>();
 	private Ini listIni = new Ini();
+	private File file;
 
-	public VocabList(String filePath) {
+	int getMaxTries() { return maxTries; }
+
+	private int maxTries;
+
+	Ini getListIni() { return listIni; }
+
+	File getFile() { return file; }
+	
+	VocabList(String filePath) {
 		try {
+			file = new File(filePath);
+			listIni.load(file);
+			Set<String> sectionNameList = listIni.keySet();
 
-			listIni = new Ini(new File(filePath));
+			//region Get starting_perfect_tries_req and max_perfect_tries_req
+			int startingPerfectTriesReq = -Integer.parseInt(listIni.fetch("preferences", "starting_perfect_tries_req"));
+			maxTries = -Integer.parseInt(listIni.fetch("preferences", "max_perfect_tries_req"));
+			//endregion
+
+			//region Add all morphs with score < 0 to morphList
+			for (String section : sectionNameList) {
+				if (!section.equals("preferences")) {
+					if (listIni.fetch(section, "score") == null) {
+						listIni.put(section, "score", startingPerfectTriesReq);
+						listIni.store(file);
+					}
+
+					if (Integer.parseInt(listIni.fetch(section, "score")) < 0) {
+						morphList.add(new Morph(section, listIni.fetch(section, "morphdef"), listIni.fetch(section, "word1"), listIni.fetch(section, "word1def"), listIni.fetch(section, "word2"), listIni.fetch(section, "word2def"), Integer.parseInt(listIni.fetch(section, "score"))));
+					}
+				}
+			}
+			//endregion
 
 		} catch (IOException e) {
 			e.printStackTrace();
